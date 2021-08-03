1e2584c51c57074b1eaad8ddbc8e2eb8f398f80e
JeffreytheCoder <2397371802@qq.com>
2021-08-03 12:42:29 +0800

    new logo & navbar

--- client/src/components/global/Navbar.js
+++ client/src/components/global/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import logoWhite from '../../images/fanspick logo white small.jpg';
+import logoWhite from '../../images/fanspick-logo-bg-white-small.jpg';
 
 const Navbar = () => {
   const [hideMenu, setMenu] = useState(true);
@@ -9,21 +9,12 @@ const Navbar = () => {
   return (
     <nav class="font-main flex items-center justify-between flex-wrap p-6 border-b-4">
       <Link to="/">
-        <div class="flex items-center flex-shrink-0 mr-6">
-          {/* <svg
-            class="fill-current h-8 w-8 mr-2"
-            width="54"
-            height="54"
-            viewBox="0 0 54 54"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-          </svg>
-          <span class="font-semibold text-xl tracking-tight">Tailwind CSS</span> */}
+        <div class="flex items-center flex-shrink-0 mr-8">
           <img className="h-10" src={logoWhite} alt="fanspick logo"></img>
-          <span class="font-bold text-xl ml-2">FansPick</span>
+          {/* <span class="font-bold text-xl ml-2">FansPick</span> */}
         </div>
       </Link>
+
       <div class="block lg:hidden">
         <button
           class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
@@ -41,6 +32,7 @@ const Navbar = () => {
           </svg>
         </button>
       </div>
+
       <div
         class={`w-full block flex-grow lg:flex lg:items-center lg:w-auto ${
           hideMenu ? 'hidden' : ''
@@ -49,23 +41,24 @@ const Navbar = () => {
         <div class="text-lg lg:flex-grow">
           <Link
             to="/my-pages"
-            class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+            class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-6"
           >
             My pages
           </Link>
           <Link
             to="/following"
-            class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+            class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-6"
           >
             Following
           </Link>
-          <Link
+          {/* <Link
             to="/login"
             class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
           >
             Login
-          </Link>
+          </Link> */}
         </div>
+
         <div>
           <Link
             to="/post"
