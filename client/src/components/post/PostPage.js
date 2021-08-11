0bc2159289a37ccc1c1838eadfaadd67f181c105
JeffreytheCoder <2397371802@qq.com>
2021-08-12 00:58:33 +0800

    following last commit

--- client/src/components/post/PostPage.js
+++ client/src/components/post/PostPage.js
@@ -81,7 +81,7 @@ const PostPage = ({
                 <div class="flex flex-row items-center mb-4">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6"
+                    className="h-5 w-5"
                     viewBox="0 0 20 20"
                     fill="currentColor"
                   >
